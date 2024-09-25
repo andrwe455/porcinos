a081198368f34a1db51f9478fa08a669a3217d58
andrwe455 <andrwe455@gmail.com>
2024-09-25 08:52:58 -0500

    bd

---
+++ Backend/database/mongo.js
@@ -0,0 +1,20 @@
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const mongoose = require('mongoose');
+const uri3 = "mongodb://0.tcp.ngrok.io:12509";
+
+function connect() {
+    return mongoose.connect(uri3)
+        .then((conn) => {
+            console.log('Conectado a la base de datos');
+            return conn;  // Devolver la conexión para su uso posterior
+        })
+        .catch((error) => {
+            console.error('Error de conexión', error);
+            throw error;  // Lanza el error para manejarlo en el nivel superior
+        });
+}
+
+module.exports = { connect, mongoose };
+
+
---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const mongo = require('./database/mongo.js');
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
+mongo.connect()
+//parseo del body
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors(({ origin: '*' })));
+app.use(express.json());
+
+
+//frontend
+const proyectPath = path.resolve(__dirname, '../Frontend/views');
+app.use(express.static(path.resolve(proyectPath)));
+
+
+// Rutas de tu API
+const router =require('./routes/routes.js');
+app.use('/', router);
