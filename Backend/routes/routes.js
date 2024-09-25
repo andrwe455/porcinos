5844f3793de52ceaf7d113fda9b03cb6a4f7166a
andrwe455 <andrwe455@gmail.com>
2024-09-25 10:57:56 -0500

    crud completo colecciones router

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -3,6 +3,19 @@ const router = Router();
 const controller = require('../controller/controller');
 
 
+router.post('/porcino', controller.createPorcino);
+router.get('/porcinos', controller.getPorcinos);
+router.get('/porcino/:identificacion', controller.getPorcino);
+router.put('/porcino/:identificacion', controller.updatePorcino);
+router.delete('/porcino/:identificacion', controller.deletePorcino);
+
+router.post('/cliente', controller.createCliente);
+router.get('/clientes', controller.getClientes);
+router.get('/cliente/:Cedula', controller.getCliente);
+router.put('/cliente/:Cedula', controller.updateCliente);
+router.delete('/cliente/:Cedula', controller.deleteCliente);
+
+
 
 
 module.exports = router;
