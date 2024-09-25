b4a1f8e02305ec0d5a0d78631cdadf2c6d415ab8
juanliferojas23 <juan_rojas82161@elpoli.edu.co>
2024-09-25 16:20:28 -0500

    adminlte

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,24 @@ const app = express();
 const path = require('path');
 const mongo = require('./database/mongo.js');
 const ejs = require('ejs');
+const express = require('express');
+const path = require('path');
+
+// Configura la carpeta de vistas
+app.set('view engine', 'ejs');
+
+// Configura la carpeta de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.render('index'); // Renderiza la vista 'index.ejs'
+});
+
+app.listen(3000, () => {
+  console.log('Servidor en puerto 3000');
+});
+
 
 
 const PORT = process.env.PORT || 3000;
